db246fa43d11c883f5f1f7087c38f2f224e437ee
Andre Dieb Martins <andre.dieb@gmail.com>
2011-06-14 03:08:34 -0300

    Exemplo de industria textil

---
+++ exemplos/Curva-Custo-Estoque.py
@@ -0,0 +1,26 @@
+import sys
+import math
+
+from Modelo import *
+
+if __name__ == '__main__':
+    c = ConfigParser.ConfigParser()
+    c.read('IndustriaTextil.in')
+    params = ParametrosSimulacao.fromConfig(c)
+
+    a = int(float(sys.argv[1]))
+    b = int(float(sys.argv[2]))
+
+    custos = []
+    capacidade = []
+    for i in range(a, b, int(math.ceil((a+b)/100.0))):
+      params.armazenamento_capacidade = i
+      saidas = Simulador(params).simular()
+
+      custo_total = sum([saidas[k] for k in saidas.keys() if k.startswith('custo_')])
+      custos.append(custo_total)
+      capacidade.append(params.armazenamento_capacidade)
+
+    print 'Y=%s;' % str(custos)
+    print 'X=%s;' % str(capacidade)
+    print 'plot(X, Y);'
